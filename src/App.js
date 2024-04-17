32f10947abf15da0a537e77e1c54737845cca1e0
Narendra <narendrach34@gmail.com>
2024-04-17 13:49:05 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import LandingPage from "./pages/LandingPage";
+
+function App() {
+  return (
+    <div className="appwrapper">
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/LandingPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./LandingPage.module.css";
+
+export default function LandingPage() {
+    return(
+        <div className={styles.wrapper}>
+            <h1>Expense Tracker</h1>
+        </div>
+    )
+}
